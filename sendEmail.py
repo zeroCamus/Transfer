ea89ce87657edb1700953874d3b86bb90c768985
WiseDoge <wisedoge@outlook.com>
2016-06-16 12:40:44 +0800

    1

---
+++ sendEmail.py
@@ -0,0 +1,19 @@
+import smtplib
+from email.mime.text import MIMEText
+'''
+def sendme(title, text):
+	title is the subject of your e-mail.
+	text is the value of your e-mail.
+'''
+def sendme(title, text):
+    msg = MIMEText(text)
+    msg['Subject'] = title
+    msg['From'] = "huohuliu@126.com"
+    msg['To'] = "fly199701@126.com"
+    s = smtplib.SMTP('smtp.126.com')
+    s.login('huohuliu', 'huohuxxttvv')
+    s.send_message(msg)
+    s.quit()
+
+if __name__ == '__main__':
+    sendme("Hello World", "Welcome")
